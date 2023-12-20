81aa050edf0cb8b4d72960db17deb033a1e08566
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-12-20 21:08:24 +0500

    made enum error

--- colorUtils.c
+++ colorUtils.c
@@ -4,17 +4,11 @@
 #include "colorUtils.h"
 
 int max(int a, int b, int c) {
-  return (a > b) ? (a > c ? a : c) : (b > c ? b: c);
+    return (a > b) ? (a > c ? a : c) : (b > c ? b : c);
 }
-
 int min(int a, int b, int c) {
-  return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+    return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
-
-int toGrayScale(int *r, int *g, int *b, Mode mode) {
-  //TODO: implement
-}
-
-int toSepia(int *r, int *g, int *b) {
-  //TODO: implement
-}
+// This function return Error and the Value of diferent filter
+Error toGrayScale(int *r, int *g, int *b, Mode mode) {
+  // if the address or refrence pass to this function as 0 it will return the value 1
